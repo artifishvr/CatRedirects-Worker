4f5f0b82fb2f20b48190c25965f2a5ea35966803
Arti <59352535+artifishvr@users.noreply.github.com>
2024-12-16 22:29:37 -0800

    fix all domains being treated as claimablw

--- src/index.js
+++ src/index.js
@@ -1,37 +1,44 @@
-import { Hono } from 'hono'
-import { eq } from 'drizzle-orm';
-import { drizzle } from 'drizzle-orm/libsql';
-import { createClient } from '@libsql/client';
-import { redirects } from './schema';
+import { Hono } from "hono";
+import { eq } from "drizzle-orm";
+import { drizzle } from "drizzle-orm/libsql";
+import { createClient } from "@libsql/client";
+import { redirects } from "./schema";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('*', async (c) => {
+app.get("*", async (c) => {
   const client = createClient({ url: c.env.DB_URL, authToken: c.env.DB_TOKEN });
 
   const db = drizzle(client);
 
-  const result = await db.select()
-    .from(redirects).where(eq(redirects.host, c.req.header('host')));
-
+  const result = await db
+    .select()
+    .from(redirects)
+    .where(eq(redirects.host, c.req.header("host")));
 
   if (!result[0]?.url) {
-    return c.text('Not found! Go to gaycat.online to claim this domain.')
-  }
-  console.log(c.req.header('host'));
-
-  if (result[0].url.endsWith('*')) {
-    let redirecturl = result[0].url.replace('/*', '')
-    redirecturl = `${redirecturl}${c.req.path}`
-
-    return c.redirect(redirecturl)
+    return c.text("Not found! Go to gaycat.online to claim this domain.");
   }
 
-  if (result[0].wkPrefix && c.req.path === `/.well-known/${result[0].wkPrefix}`) {
-    return c.text(result[0].wkContent)
+  if (result[0].url == "reserved") {
+    return;
+  }
+
+  if (result[0].url.endsWith("*")) {
+    let redirecturl = result[0].url.replace("/*", "");
+    redirecturl = `${redirecturl}${c.req.path}`;
+
+    return c.redirect(redirecturl);
+  }
+
+  if (
+    result[0].wkPrefix &&
+    c.req.path === `/.well-known/${result[0].wkPrefix}`
+  ) {
+    return c.text(result[0].wkContent);
   }
 
   return c.redirect(result[0].url);
-})
+});
 
-export default app
+export default app;
